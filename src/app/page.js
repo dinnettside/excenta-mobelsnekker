04f5f4b1aaca2c28c30cebf0c6a4c416c76c9818
dinnettside <trym*dinnettside.no>
2025-07-29 20:22:55 +0200

    Oppdaterte globals.css, tailwind.config.js og page.js for ny bakgrunn og font

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import CardStackHero from './CardStackHero';
+import CardStackHero from '@/components/CardStackHero'
 
 export default function Home() {
   return (
     <main>
       <CardStackHero />
-      {/* ...resten av innholdet... */}
+      {/* Flere seksjoner kommer under */}
     </main>
-  );
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        section: '#f7f5ef', // lys kremfarge som Interiora
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
