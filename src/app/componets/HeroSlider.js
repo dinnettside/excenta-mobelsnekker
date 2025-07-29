da40936baf42a58bc0b3e0b2097864037df40bbf
dinnettside <trym*dinnettside.no>
2025-07-29 19:19:36 +0200

    Fikset sti og lagt til HeroSlider.js riktig

--- src/app/componets/HeroSlider.js
+++ src/app/componets/HeroSlider.js
@@ -5,18 +5,8 @@ import Image from 'next/image';
 const images = ['hero.webp', 'hero5.webp', 'hero6.webp'];
 
 export default function HeroSlider() {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % images.length);
-    }, 5000); // bytt hvert 5. sekund
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="absolute inset-0 z-0 opacity-50">
+    <div className="absolute inset-0 z-0 opacity-60">
       <img src="/images/hero1.webp" alt="Hero" className="w-full h-full object-cover" />
     </div>
   );
